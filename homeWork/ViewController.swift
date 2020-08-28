aa4695660b7fc3b9eefe4db17be9df6e51974435
Nadia <nadzjeja.b@gmail.com>
2020-08-28 13:50:35 +0300

    Initial Commit

---
+++ homeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homeWork
+//
+//  Created by Nadia on 8/28/20.
+//  Copyright © 2020 Nadia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
